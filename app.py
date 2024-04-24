73e4756d54f8d653bf84eea12349db5281a53d28
zhalok-avenirip <zhalok@aveniripltd.com>
2024-04-24 11:38:56 +0600

    initialized firebase app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends
 
 from auth.jwt_bearer import JWTBearer
-from config.config import initiate_database
+from config.config import initiate_database,iinitiate_firebase
 from routes.generate import GenerateRouter
+
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ token_listener = JWTBearer()
 @app.on_event("startup")
 async def start_database():
     await initiate_database()
+    await iinitiate_firebase()
 
 
 @app.get("/", tags=["Root"])
@@ -19,6 +21,5 @@ async def read_root():
     return {"message": "Welcome to this fantastic app."}
 
 
-# app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
-# app.include_router(StudentRouter,tags=["Students"],prefix="/student",dependencies=[Depends(token_listener)],)
+
 app.include_router(GenerateRouter,tags=["Generation"],prefix="/generate",)
--- config/config.py
+++ config/config.py
@@ -4,12 +4,19 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic_settings import BaseSettings
 import models as models
+import firebase_admin
+
+
 
 
 class Settings(BaseSettings):
     # database configurations
     DATABASE_URL: Optional[str] = None
     HF_API_KEY:str 
+    # FIREBASE_PRIVATE_KEY_ID:str
+    FIREBASE_PRIVATE_KEY:str
+    FIREBASE_PROJECT_ID:str
+    FIREBASE_CLIENT_EMAIL:str
 
     # JWT
     secret_key: str = "secret"
@@ -20,8 +27,27 @@ class Settings(BaseSettings):
         from_attributes = True
 
 
+
+
 async def initiate_database():
     client = AsyncIOMotorClient(Settings().DATABASE_URL)
     await init_beanie(
         database=client.get_default_database(), document_models=models.__all__
     )
+
+
+async def iinitiate_firebase():
+    env = Settings()
+    config = {
+    "type":"service_account",
+    "client_email": env.FIREBASE_CLIENT_EMAIL,
+    "project_id": env.FIREBASE_PROJECT_ID,
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "private_key": env.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
+   
+    }
+
+
+    cred = firebase_admin.credentials.Certificate(config)
+    firebase_admin.initialize_app(cred,{ "storageBucket":"learnyx-962bf.appspot.com"})
+    
